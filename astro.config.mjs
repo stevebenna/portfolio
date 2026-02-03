8bbe2d61d159da91800c54bbe4410bb4fda42071
Stefano <s.benatti@rewindmail.it>
2026-02-03 13:56:16 +0100

    upd: custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://stevebenna.github.io",
+    site: "https://stivv.it",
     base: "/",
 });
